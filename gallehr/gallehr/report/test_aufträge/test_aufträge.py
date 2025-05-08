4eccbefb33cca4d7e186c3f5499ad5e3b7d57642
beingeek <furqan.790@outlook.com>
2025-05-08 11:59:06 +0000

    feat: add report files for Test Aufträge, Review, and Money Plex Export Test

---
+++ gallehr/gallehr/report/test_aufträge/test_aufträge.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, phamos.eu and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
