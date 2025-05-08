9d00b6a888c2c3adbdd39f9ddbc006b4c00ec8f2
beingeek <furqan.790@outlook.com>
2025-05-08 12:32:10 +0000

    refactor: Test Aufträge ass non standard script report

--- gallehr/gallehr/report/test_aufträge/test_aufträge.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, phamos.eu and contributors
-# For license information, please see license.txt
-
-# import frappe
-
-
-def execute(filters=None):
-	columns, data = [], []
-	return columns, data
